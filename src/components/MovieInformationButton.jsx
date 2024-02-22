94f746fde29f2df9d7fba6223be0a2f90585071e
kat-scragg <katlan.scragg@gmail.com>
2024-02-22 15:01:00 -0800

    adds chackra icon buttons into npm and changes movie information button to be the info icon

--- src/components/MovieInformationButton.jsx
+++ src/components/MovieInformationButton.jsx
@@ -1,19 +1,20 @@
 import { Link } from 'react-router-dom';
-import { Button } from '@chakra-ui/react';
+import { IconButton } from '@chakra-ui/react';
+import { InfoOutlineIcon } from '@chakra-ui/icons'; // Import the icon
 
 export const MovieInformationButton = ({ movieId }) => {
   return (
     <Link to={`/movie-details/${movieId}`}>
-      <Button
+      <IconButton
+        aria-label="Movie Information"
+        icon={<InfoOutlineIcon />} // Use the InfoOutlineIcon as the icon
         bg="blue.500"
         color="white"
         rounded="md"
         fontWeight="bold"
         _hover={{ bg: 'blue.600' }}
         _focus={{ outline: 'none' }}
-      >
-        More Info
-      </Button>
+      />
     </Link>
   );
 };
