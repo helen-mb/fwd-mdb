498d494f2beeb649001cccc959c7758019691f9c
kat-scragg <katlan.scragg@gmail.com>
2024-02-22 16:03:55 -0800

    adjusts and removes redundant code in FavouritesButton

--- src/components/FavouritesButton.jsx
+++ src/components/FavouritesButton.jsx
@@ -5,42 +5,31 @@ import { AddIcon } from '@chakra-ui/icons'; // Import the AddIcon
 export const FavouritesButton = ({ movieId, onFavouriteChange }) => {
   const localStorageKey = `favorite_${movieId}`;
 
-  // State to track whether the movie is added to favorites
   const [isFavourite, setIsFavourite] = useState(() => {
-    // Retrieve the favorited status from localStorage
-    const isFav = localStorage.getItem(localStorageKey);
-    // Convert the stored value to a boolean, default to false if not found
-    return isFav === 'true';
+    return localStorage.getItem(localStorageKey) === 'true';
   });
 
-  // function to set the initial value in local storage so that the button is in sync with the local storage colour wise and value wise
   useEffect(() => {
-    // If the movie is favourited initially, set it in local storage
     if (isFavourite) {
       localStorage.setItem(localStorageKey, 'true');
     }
   }, [isFavourite, localStorageKey]);
 
-  // Function to toggle the favorite status
   const toggleFavourite = () => {
     const updatedIsFavourite = !isFavourite;
     setIsFavourite(updatedIsFavourite);
 
-    // Update the favorited status in localStorage
-     localStorage.setItem(localStorageKey, updatedIsFavourite.toString());
+    localStorage.setItem(localStorageKey, updatedIsFavourite.toString());
 
-    // Update favouritedMovies array in local storage
-    let favouritedMovies = JSON.parse(localStorage.getItem('favouritedMovies')) || [];
-    if (updatedIsFavourite) {
-      favouritedMovies.push(movieId);
-    } else {
-      favouritedMovies = favouritedMovies.filter(id => id !== movieId);
-    }
-    localStorage.setItem('favouritedMovies', JSON.stringify([...new Set(favouritedMovies)]));
+    const favouritedMovies = JSON.parse(localStorage.getItem('favouritedMovies')) || [];
+    const updatedFavouritedMovies = updatedIsFavourite
+      ? [...new Set([...favouritedMovies, movieId])]
+      : favouritedMovies.filter(id => id !== movieId);
 
-    // Call onFavouriteChange callback with updated favouritedMovies array
+    localStorage.setItem('favouritedMovies', JSON.stringify(updatedFavouritedMovies));
+
     if (onFavouriteChange) {
-      onFavouriteChange(favouritedMovies);
+      onFavouriteChange(updatedFavouritedMovies);
     }
   };
 
