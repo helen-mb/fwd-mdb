b90499c463d29be912344a35a41eb4faddc88eb7
kat-scragg <katlan.scragg@gmail.com>
2024-02-22 16:08:46 -0800

    adds some comments into the favourites button

--- src/components/FavouritesButton.jsx
+++ src/components/FavouritesButton.jsx
@@ -2,25 +2,30 @@ import { useState, useEffect } from 'react';
 import { IconButton } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons'; // Import the AddIcon
 
+// Create a new component called FavouritesButton
 export const FavouritesButton = ({ movieId, onFavouriteChange }) => {
   const localStorageKey = `favorite_${movieId}`;
-
+  
+  // this hook will check if the movie is already in the favourites list
   const [isFavourite, setIsFavourite] = useState(() => {
     return localStorage.getItem(localStorageKey) === 'true';
   });
 
+  // this hook will update the local storage when the isFavourite state changes
   useEffect(() => {
     if (isFavourite) {
       localStorage.setItem(localStorageKey, 'true');
     }
   }, [isFavourite, localStorageKey]);
 
+  // this function will toggle the isFavourite state and update the local storage
   const toggleFavourite = () => {
     const updatedIsFavourite = !isFavourite;
     setIsFavourite(updatedIsFavourite);
 
     localStorage.setItem(localStorageKey, updatedIsFavourite.toString());
 
+    // Update the list of favourited movies in localStorage
     const favouritedMovies = JSON.parse(localStorage.getItem('favouritedMovies')) || [];
     const updatedFavouritedMovies = updatedIsFavourite
       ? [...new Set([...favouritedMovies, movieId])]
@@ -28,6 +33,7 @@ export const FavouritesButton = ({ movieId, onFavouriteChange }) => {
 
     localStorage.setItem('favouritedMovies', JSON.stringify(updatedFavouritedMovies));
 
+    // Call the onFavouriteChange callback if it exists
     if (onFavouriteChange) {
       onFavouriteChange(updatedFavouritedMovies);
     }
