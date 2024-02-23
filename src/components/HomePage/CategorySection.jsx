f8364482d5a252b7f91048a9c1fab397281656b0
Helen Burger <burger.helen.m@gmail.com>
2024-02-23 00:32:23 -0800

    Moved API call to context at app router level.

--- src/components/HomePage/CategorySection.jsx
+++ src/components/HomePage/CategorySection.jsx
@@ -1,38 +1,14 @@
 import { Box, Grid } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { MovieQuickInfo } from '../MovieQuickInfo';
+import { DataContext } from '../../Contexts';
 
-export const CategorySection = ({ categories }) => {
-  const [moviesByCategory, setMoviesByCategory] = useState({});
-
-  useEffect(() => {
-    const fetchMoviesByCategory = async () => {
-      try {
-        const promises = categories.map(async (category) => {
-          const apiUrl = `https://api.themoviedb.org/3/movie/${category}?api_key=${
-            import.meta.env.VITE_REACT_APP_TMDB_API_KEY
-          }`;
-          const response = await fetch(apiUrl);
-          const data = await response.json();
-          return { category, movies: data.results.slice(0, 6) };
-        });
-        const moviesData = await Promise.all(promises);
-        const moviesByCategoryObj = {};
-        moviesData.forEach(({ category, movies }) => {
-          moviesByCategoryObj[category] = movies;
-        });
-        setMoviesByCategory(moviesByCategoryObj);
-      } catch (error) {
-        console.error('Error fetching movie data:', error);
-      }
-    };
-
-    fetchMoviesByCategory();
-  }, [categories]);
+export const CategorySection = () => {
+  const movieData = useContext(DataContext);
 
   return (
     <Box>
-      {Object.entries(moviesByCategory).map(([category, movies]) => (
+      {Object.entries(movieData).map(([category, movies]) => (
         <Box key={category}>
           <h2>{category}</h2>
           <Grid templateColumns="repeat(6, 1fr)" gap={6}>
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -5,23 +5,80 @@ import { About } from '../pages/About';
 import { MyList } from '../pages/MyList';
 import { Error } from '../pages/Error';
 import Header from '../components/Header';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { DataContext } from '../Contexts';
 
 export const AppRouter = () => {
+  const categories = ['popular', 'top_rated', 'upcoming', 'now_playing'];
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchMoviesByCategory = async () => {
+      try {
+        const promises = categories.map(async (category) => {
+          const apiUrl = `https://api.themoviedb.org/3/movie/${category}?api_key=${
+            import.meta.env.VITE_REACT_APP_TMDB_API_KEY
+          }`;
+          const response = await fetch(apiUrl);
+          const data = await response.json();
+          return { category, movies: data.results.slice(0, 6) };
+        });
+        const moviesData = await Promise.all(promises);
+        const moviesByCategoryObj = {};
+        moviesData.forEach(({ category, movies }) => {
+          moviesByCategoryObj[category] = movies;
+        });
+        setData(moviesByCategoryObj);
+      } catch (error) {
+        console.error('Error fetching movie data:', error);
+      }
+    };
+
+    fetchMoviesByCategory();
+  }, []);
+
+  // useEffect(() => {
+  //   // api call -> setData()
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       accept: 'application/json',
+  //     },
+  //   };
+  //   const category = 'now_playing';
+  //   fetch(
+  //     `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1&api_key=${
+  //       import.meta.env.VITE_REACT_APP_TMDB_API_KEY
+  //     }`,
+  //     options
+  //   )
+  //     .then((response) => response.json())
+  //     // .then((response) => console.log(response.results))
+  //     .then((response) => setData(response.results))
+  //     .catch((err) => console.error(err));
+  // }, []);
+
+  if (!data) {
+    return <></>;
+  }
   return (
-    <BrowserRouter>
-      <div className="siteWrapper">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/movie-details/:id" element={<MovieDetails />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/my-list" element={<MyList />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </main>
-        {/* Footer component */}
-      </div>
-    </BrowserRouter>
+    <DataContext.Provider value={data}>
+      <BrowserRouter>
+        <div className="siteWrapper">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/movie-details/:id" element={<MovieDetails />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/my-list" element={<MyList />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </main>
+          {/* Footer component */}
+        </div>
+      </BrowserRouter>
+    </DataContext.Provider>
   );
 };
