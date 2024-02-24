99d108f4d78d576f64c6983c4f765824f0e2ecfc
kat-scragg <katlan.scragg@gmail.com>
2024-02-23 17:05:04 -0800

    adds links to the images in the about page

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -1,11 +1,12 @@
 // React Imports
 import { useEffect } from 'react';
 // Chakra UI Imports
-import { Box, Text, Heading } from '@chakra-ui/react';
+import { Box, Text, Heading, Link } from '@chakra-ui/react';
 // Components
 import { StaticBanner } from '../StaticBanner';
 import tmdbLogo from '../../assets/tmdb-logo.svg';
 import takeFiveLogo from '../../assets/take-five-logo.svg';
+import { Link as RouterLink } from 'react-router-dom';
 
 export const AboutPage = () => {
   useEffect(() => {
@@ -21,7 +22,9 @@ export const AboutPage = () => {
         
         <Box mb={8}>
           <Heading as="h2" size="lg" mb={4}>What is Take Five?</Heading>
-          <img src={takeFiveLogo} alt="Take Five Logo" style={{width:'200px', height: '200px'}}/>
+          <Link as={RouterLink} to="/">
+            <img src={takeFiveLogo} alt="Take Five Logo" style={{width:'200px', height: '200px'}}/>
+          </Link>
           <Text mb={4}>
             Scene Dock is a web-app designed to help you keep your movie watch-list up-to-date and organized. Stay on top of what movies are new and popular and add movies to your list where you can track what you have and have not yet seen.
           </Text>
@@ -29,7 +32,11 @@ export const AboutPage = () => {
         
         <Box mb={8}>
           <Heading as="h2" size="lg" mb={4}>What is the Movie Database?</Heading>
-          <img src={tmdbLogo} alt="The Movie Database Logo" style={{width:'200px', height: '200px'}}/>
+
+          <Link href= "https://www.themoviedb.org/" isExternal>
+            <img src={tmdbLogo} alt="The Movie Database Logo" style={{width:'200px', height: '200px'}}/>
+          </Link>
+
           <Text mb={4}>
             Scene Dock gets all its movie data and images from The Movie Database (TMDB), a community-built movie and TV database. The TMDB API (application programming interface) is a system that allows Scene Dock to programmatically keep its information up-to-date. Read more about TMDB on their website.
           </Text>
