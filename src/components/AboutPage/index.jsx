42749f3c0bcadcb9ebf5c7692ba43d2f4dff31f5
kat-scragg <katlan.scragg@gmail.com>
2024-02-23 16:56:30 -0800

    adds logo images into about code and does inline style for them. Also adds background colour to make visible

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 import { Box, Text, Heading } from '@chakra-ui/react';
 // Components
 import { StaticBanner } from '../StaticBanner';
+import tmdbLogo from '../../assets/tmdb-logo.svg';
+import takeFiveLogo from '../../assets/take-five-logo.svg';
 
 export const AboutPage = () => {
   useEffect(() => {
@@ -13,13 +15,13 @@ export const AboutPage = () => {
   return (
     <>
       <StaticBanner />
-      <Box p={4}>
+      <Box p={4} bg="gray.500">
         <Heading as="h1" size="xl" mb={2}>About Take Five</Heading>
         <Text fontSize="lg" mb={4}>and the Movie Database API</Text>
         
         <Box mb={8}>
           <Heading as="h2" size="lg" mb={4}>What is Take Five?</Heading>
-          {/* Logo component */}
+          <img src={takeFiveLogo} alt="Take Five Logo" style={{width:'200px', height: '200px'}}/>
           <Text mb={4}>
             Scene Dock is a web-app designed to help you keep your movie watch-list up-to-date and organized. Stay on top of what movies are new and popular and add movies to your list where you can track what you have and have not yet seen.
           </Text>
@@ -27,7 +29,7 @@ export const AboutPage = () => {
         
         <Box mb={8}>
           <Heading as="h2" size="lg" mb={4}>What is the Movie Database?</Heading>
-          {/* Logo component */}
+          <img src={tmdbLogo} alt="The Movie Database Logo" style={{width:'200px', height: '200px'}}/>
           <Text mb={4}>
             Scene Dock gets all its movie data and images from The Movie Database (TMDB), a community-built movie and TV database. The TMDB API (application programming interface) is a system that allows Scene Dock to programmatically keep its information up-to-date. Read more about TMDB on their website.
           </Text>
@@ -35,7 +37,7 @@ export const AboutPage = () => {
         
         {/* Disclaimer section */}
         <Text mb={4}>
-          Disclaimer: [Your disclaimer text goes here]
+          Disclaimer: This product uses the TMDb API but is not endorsed or certified by TMDb.
         </Text>
       </Box>
     </>
