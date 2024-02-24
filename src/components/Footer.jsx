4ae58109680df1d75aff18bd96ee28f0dab650d6
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-02-23 20:28:24 -0800

    Fixes file path and adds brand color to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,16 @@ import { Box, Text} from '@chakra-ui/react';
 
 // Components
 import { Nav } from './Nav';
-// import takeFiveLogo from '../assets/take-five-logo.png';
-// import ReactRouterLink from 'react-router-dom/Link';
+import takeFiveLogo from '../assets/take-five-logo.svg';
+import {Link as ReactRouterLink} from 'react-router-dom';
+
 
 const Footer = () => {
   return (
-    <Box bg="tomato" w="100%" p={4} color="white">
-      {/* <Link as={ReactRouterLink} to="/">
+    <Box bg="brand.900" w="100%" p={4} color="white">
+      <ReactRouterLink to="/">
         <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" />
-      </Link> */}
+      </ReactRouterLink>
       <Text>
         <span>&#169;</span> 2024 Burger, Engstrom, and Scragg
       </Text>
