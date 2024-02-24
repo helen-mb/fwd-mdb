c99efabc94797746921e4b537b489bf0017445d0
kat-scragg <katlan.scragg@gmail.com>
2024-02-23 17:51:53 -0800

    uncomments everything out for merge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,18 @@
 // React Imports
 // Chakra UI Imports
-import { Box } from '@chakra-ui/react';
-// will have to add link to import once merged
+import { Box, Link} from '@chakra-ui/react';
 
 // Components
 import { Nav } from './Nav';
-// import takeFiveLogo from 'add in correct path';
-// import ReactRouterLink from 'react-router-dom/Link';
+import takeFiveLogo from '../../assets/take-five-logo.png';
+import ReactRouterLink from 'react-router-dom/Link';
 
 const Header = () => {
   return (
     <Box bg="tomato" w="100%" p={4} color="white">
-      {/* <Link as={ReactRouterLink} to="/">
+      <Link as={ReactRouterLink} to="/">
         <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" />
-      </Link> */}
+      </Link>
       <Nav />
     </Box>
   );
