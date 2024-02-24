4755fd3995a4bb5c74de714d79c424d7e5831ec5
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-02-23 21:40:32 -0800

    Realigns header flex items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,18 @@ import {Link as ReactRouterLink} from 'react-router-dom';
 
 const Header = () => {
   return (
-    <Box bg="brand.900" w="100%" p={4}>
-      <ReactRouterLink to="/">
-        <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" />
-      </ReactRouterLink>
-      <Nav />
+    <Box bg="brand.900" w="100%" px={5} py={4}>
+      <Flex align="center">
+        <Box>
+          <ReactRouterLink to="/">
+            <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" />
+          </ReactRouterLink>
+        </Box>
+        <Spacer></Spacer>
+        <Box>
+          <Nav />
+        </Box>
+      </Flex>
     </Box>
   );
 };
