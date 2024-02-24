cbb24fb232655268b57a202ecfc4e7f0cb65cb87
kat-scragg <katlan.scragg@gmail.com>
2024-02-24 12:03:31 -0800

    adds conditional background image for static banner. Pulls in unique movie list

--- src/components/StaticBanner.jsx
+++ src/components/StaticBanner.jsx
@@ -4,29 +4,26 @@ import { useContext } from 'react';
 import { Box, Text } from '@chakra-ui/react';
 // Components
 import { DataContext } from '../Contexts';
+import { getUniqueMovieList } from '../utils';
 
-
-export const StaticBanner = () => {
-
+export const StaticBanner = ({movieId}) => {
   const movieData = useContext(DataContext);
-  for (const category in movieData) {
-    movieData[category].forEach((movieItem) => {
-      console.log(movieItem);
-      //if movieItem.id === id of movie page
-      //then grab movieItem.backdrop_path
-    });
-  }
-// if the page has a movie id passed to it, then execute the above logic
-
-//else
-
-//us decorative backgoround
+  const uniqueMovie= getUniqueMovieList(movieData);
 
 
+  const movie = uniqueMovie.find((movieItem) => movieItem.id === parseInt(movieId));
+  const backdropImage = movie?.backdrop_path;
+  
   return (
     <>
       {/* Hero Image */}
-      <Box borderWidth={'1px'} bg={'lightblue'}>
+      <Box bg={backdropImage ? `url(https://image.tmdb.org/t/p/w1280${backdropImage})` : 'purple'}
+
+      //inline style below that will have to be changed for bg image
+      style={{
+        height: '400px',
+        backgroundSize: 'cover'
+      }}>
         {/* Hero Image */}
         <Text>This is a banner. A hero image will go here.</Text>
         <Box>
