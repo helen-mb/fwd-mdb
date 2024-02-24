2d6b333384ecbcc77783a5733783a7d5f87c475e
kat-scragg <katlan.scragg@gmail.com>
2024-02-23 17:43:54 -0800

    removes 'header' tag from header and adds in take five logo image but comments it out.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 // React Imports
 // Chakra UI Imports
-import { Box, Heading } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 // Components
 import { Nav } from './Nav';
+// import takeFiveLogo from 'add in correct path';
 
 const Header = () => {
   return (
     <Box bg="tomato" w="100%" p={4} color="white">
-      <Heading as="h1">Take Five</Heading>
+      {/* <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" /> */}
       <Nav />
     </Box>
   );
