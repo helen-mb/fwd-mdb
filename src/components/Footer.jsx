4b5e407b2f7a4e17627aac468155d1acee5bdee4
kat-scragg <katlan.scragg@gmail.com>
2024-02-23 17:49:22 -0800

    adds in link to home page for footer and header take five logo. comments it out

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
 // React Imports
 // Chakra UI Imports
 import { Box, Text} from '@chakra-ui/react';
+// will  have to add link once merged
 // Components
 import { Nav } from './Nav';
-// import takeFiveLogo from '../ass';
+// import takeFiveLogo from 'add in correct path';
+// import ReactRouterLink from 'react-router-dom/Link';
 
 const Footer = () => {
   return (
     <Box bg="tomato" w="100%" p={4} color="white">
-      {/* <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" /> */}
+      {/* <Link as={ReactRouterLink} to="/">
+        <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" />
+      </Link> */}
       <Text>
         <span>&#169;</span> 2024 Burger, Engstrom, and Scragg
       </Text>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 // React Imports
 // Chakra UI Imports
 import { Box } from '@chakra-ui/react';
+// will have to add link to import once merged
+
 // Components
 import { Nav } from './Nav';
 // import takeFiveLogo from 'add in correct path';
+// import ReactRouterLink from 'react-router-dom/Link';
 
 const Header = () => {
   return (
     <Box bg="tomato" w="100%" p={4} color="white">
-      {/* <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" /> */}
+      {/* <Link as={ReactRouterLink} to="/">
+        <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" />
+      </Link> */}
       <Nav />
     </Box>
   );
