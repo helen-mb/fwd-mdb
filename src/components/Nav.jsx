654614f736dbe8eea209964855cc9c12c45d1c93
kat-scragg <katlan.scragg@gmail.com>
2024-02-23 17:28:40 -0800

    figures out chakra menu. Comments that out for mobile later. Adds flex and link for chakra menu items

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 // React Imports
 // Chakra UI Imports
-import { Box, Text, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
+import { Box, Flex, Link } from '@chakra-ui/react';
 // Components
 import { NavLink } from 'react-router-dom';
 
@@ -12,22 +12,38 @@ export const Nav = () => {
   }
   return (
     <Box as="nav" className="main-nav" onClick={blur}>
-      <Menu>
-        <MenuButton as={Text} cursor="pointer">
-          Menu
-        </MenuButton>
-        <MenuList>
-          <MenuItem>
-            <NavLink to="/">Home</NavLink>
-          </MenuItem>
-          <MenuItem>
-            <NavLink to="/about">About</NavLink>
-          </MenuItem>
-          <MenuItem>
-            <NavLink to="/my-list">My List</NavLink>
-          </MenuItem>
-        </MenuList>
-      </Menu>
+      <Flex>
+        <Link as={NavLink} to="/" mr={4} fontWeight="bold">
+          Home
+        </Link>
+        <Link as={NavLink} to="/about" mr={4}>
+          About
+        </Link>
+        <Link as={NavLink} to="/my-list">
+          My List
+        </Link>
+      </Flex>
     </Box>
   );
 };
+
+// this menu layout gives you a dropdown menu so we may need to use it for mobile
+
+{/* <Box as="nav" className="main-nav" onClick={blur}>
+<Menu>
+  <MenuButton as={Text} cursor="pointer">
+    Menu
+  </MenuButton>
+  <MenuList>
+    <MenuItem>
+      <NavLink to="/">Home</NavLink>
+    </MenuItem>
+    <MenuItem>
+      <NavLink to="/about">About</NavLink>
+    </MenuItem>
+    <MenuItem>
+      <NavLink to="/my-list">My List</NavLink>
+    </MenuItem>
+  </MenuList>
+</Menu>
+</Box> */}
