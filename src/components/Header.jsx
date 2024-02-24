a1205e18582eda83dc32176e9c14c54190f89456
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-02-23 20:28:07 -0800

    Fixes logo path and adds brand color to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
 // React Imports
 // Chakra UI Imports
-import { Box} from '@chakra-ui/react';
+import { Box, Grid, GridItem } from '@chakra-ui/react';
 
 // Components
 import { Nav } from './Nav';
-// import takeFiveLogo from '../assets/take-five-logo.png';
-// import ReactRouterLink from 'react-router-dom/Link';
+import takeFiveLogo from '../assets/take-five-logo.svg';
+import {Link as ReactRouterLink} from 'react-router-dom';
 
 const Header = () => {
   return (
-    <Box bg="tomato" w="100%" p={4} color="white">
-      {/* <Link as={ReactRouterLink} to="/">
+    <Box bg="brand.900" w="100%" p={4}>
+      <ReactRouterLink to="/">
         <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" />
-      </Link> */}
+      </ReactRouterLink>
       <Nav />
     </Box>
   );
