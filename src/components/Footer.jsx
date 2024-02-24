97fd266871e420aa4f58478028c66365aa890231
kat-scragg <katlan.scragg@gmail.com>
2024-02-23 18:02:26 -0800

    just removes images for now in footer and header. I'll fix later:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
 // React Imports
 // Chakra UI Imports
-import { Box, Text, Link} from '@chakra-ui/react';
+import { Box, Text} from '@chakra-ui/react';
 
 // Components
 import { Nav } from './Nav';
-import takeFiveLogo from '../assets/take-five-logo.png';
-import ReactRouterLink from 'react-router-dom/Link';
+// import takeFiveLogo from '../assets/take-five-logo.png';
+// import ReactRouterLink from 'react-router-dom/Link';
 
 const Footer = () => {
   return (
     <Box bg="tomato" w="100%" p={4} color="white">
-      <Link as={ReactRouterLink} to="/">
+      {/* <Link as={ReactRouterLink} to="/">
         <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" />
-      </Link>
+      </Link> */}
       <Text>
         <span>&#169;</span> 2024 Burger, Engstrom, and Scragg
       </Text>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
 // React Imports
 // Chakra UI Imports
-import { Box, Link} from '@chakra-ui/react';
+import { Box} from '@chakra-ui/react';
 
 // Components
 import { Nav } from './Nav';
-import takeFiveLogo from '../assets/take-five-logo.png';
-import ReactRouterLink from 'react-router-dom/Link';
+// import takeFiveLogo from '../assets/take-five-logo.png';
+// import ReactRouterLink from 'react-router-dom/Link';
 
 const Header = () => {
   return (
     <Box bg="tomato" w="100%" p={4} color="white">
-      <Link as={ReactRouterLink} to="/">
+      {/* <Link as={ReactRouterLink} to="/">
         <img src={takeFiveLogo} alt="Take Five Logo" width="100" height="100" />
-      </Link>
+      </Link> */}
       <Nav />
     </Box>
   );
