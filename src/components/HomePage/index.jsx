ce3650407d6f3185be4d4c360e910d588141778e
Helen Burger <burger.helen.m@gmail.com>
2024-02-24 19:24:14 -0800

    Add Links on home to act as jump links in future

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,13 +1,34 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, HStack } from '@chakra-ui/react';
 import { CategorySection } from './CategorySection';
 import { BannerCarousel } from './BannerCarousel';
+import { Link as ReactRouterLink } from 'react-router-dom';
+import { Link as ChakraLink } from '@chakra-ui/react';
 
 export const HomePage = () => {
   return (
-    <Box>
-      {/* BannerCarousel */}
-      <BannerCarousel />
-      <CategorySection />
-    </Box>
+    <>
+      <Box>
+        <BannerCarousel />
+        <Heading textAlign={'center'} m={5}>
+          {/* Skip Links */}
+          Discover
+        </Heading>
+        <HStack>
+          <ChakraLink as={ReactRouterLink} to="#popular">
+            Popular
+          </ChakraLink>
+          <ChakraLink as={ReactRouterLink} to="#top_rated">
+            Top Rated
+          </ChakraLink>
+          <ChakraLink as={ReactRouterLink} to="#upcoming">
+            Upcoming
+          </ChakraLink>
+          <ChakraLink as={ReactRouterLink} to="#now_playing">
+            Now Playing
+          </ChakraLink>
+        </HStack>
+        <CategorySection />
+      </Box>
+    </>
   );
 };
