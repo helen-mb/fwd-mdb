decae354336889450e76603d61158de5da608a51
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-02-25 06:52:50 -0800

    Adjusts grid columns

--- src/components/MyListPage/index.jsx
+++ src/components/MyListPage/index.jsx
@@ -27,9 +27,11 @@ export const MyListPage = () => {
         <Grid
           templateColumns={[
             'repeat(1, 1fr)',
+            'repeat(2, 1fr)',
             'repeat(3, 1fr)',
             'repeat(4, 1fr)',
             'repeat(5, 1fr)',
+            'repeat(6, 1fr)',
           ]}
           gap={6}
         >
