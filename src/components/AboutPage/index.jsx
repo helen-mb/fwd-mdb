3a535557539d472c3993f513bc088a5c68d2e46c
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-02-25 04:03:03 -0800

    Adds styling for about page banner

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -1,37 +1,64 @@
 // React Imports
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 // Chakra UI Imports
-import { Box, Text, Heading, Link, Flex } from '@chakra-ui/react';
+import { Box, Text, Heading, Link, Flex, Grid } from '@chakra-ui/react';
 // Components
 import tmdbLogo from '../../assets/tmdb-logo.svg';
-import takeFiveLogo from '../../assets/take-five-logo.svg';
+import takeFiveLogo from '../../assets/take-five-logo-remake.svg';
 import { Link as RouterLink } from 'react-router-dom';
 import navTextStyles from '../../themes/nav-themes.jsx';
 
 export const AboutPage = () => {
+  // const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
   useEffect(() => {
     document.title = 'About | Take Five';
+  //   const handleResize = () => {
+  //     setViewportWidth(window.innerWidth);
+  //   };
+  //   window.addEventListener('resize', handleResize);
+  //   // Cleanup the event listener on component unmount
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   };
   }, []);
+
+  // let flexDirection = "column";
+  // let logoWidth = "100%";
+
+  // if(viewportWidth < 500) {
+
+  // }
+
+  // if(viewportWidth < 768) {
+
+  // }
+
+  // if (viewportWidth > 1080) {
+
+  // }
 
   return (
     <>
       <Box bg="brand.900" color="brand.50">
-        <Flex direction="column" fontFamily="assistant.normal" borderTop="2px solid #ffffff22" borderBottom="2px solid #ffffff22"p="5" px="6rem" gap="5">
+        <Flex direction="column" fontFamily="assistant.normal" borderTop="2px solid #ffffff22" borderBottom="2px solid #ffffff22"p="5" gap="5">
           <Heading as="h1" size="3xl" mb={2} fontFamily="assistant.normal">About Take Five</Heading>
           <Text fontSize="lg">and <Link href= "https://www.themoviedb.org/" isExternal sx={navTextStyles} borderBottom="2px solid #1962B6">the Movie Database API</Link></Text>
         </Flex>
         
-        <Box mb={8} p='4'>
-          <Heading as="h2" size="lg" mb={4}>What is Take Five?</Heading>
-          <Link as={RouterLink} to="/">
-            <img src={takeFiveLogo} alt="Take Five Logo" style={{width:'200px', height: '200px'}}/>
-          </Link>
-          <Text mb={4}>
-            Take Five is a web-app designed to help you keep your movie watch-list up-to-date and organized. Stay on top of what movies are new and popular and add movies to your list where you can track what you have and have not yet seen.
-          </Text>
+        <Box mb={8} p='4' fontFamily="assistant.normal">
+            <Link as={RouterLink} to="/" >
+              <img src={takeFiveLogo} alt="Take Five Logo" width="100%"/>
+            </Link>
+          <Heading as="h2" size="lg" fontFamily="assistant.normal">What is Take Five?</Heading>
+          <Flex justify="center" direction="column" gap="1rem">
+            
+            <Text>
+              Take Five is a web-app designed to help you keep your movie watch-list up-to-date and organized. Stay on top of what movies are new and popular and add movies to your list where you can track what you have and have not yet seen.
+            </Text>
+          </Flex>
         </Box>
         
-        <Box mb={8}>
+        <Box mb={8} p='4'>
           <Heading as="h2" size="lg" mb={4}>What is the Movie Database?</Heading>
 
           <Link href= "https://www.themoviedb.org/" isExternal>
@@ -44,7 +71,7 @@ export const AboutPage = () => {
         </Box>
         
         {/* Disclaimer section */}
-        <Text mb={4}>
+        <Text>
           Disclaimer: This product uses the TMDb API but is not endorsed or certified by TMDb.
         </Text>
       </Box>
