96d0b0b43ac77550d56a2c1637002eebf65a2562
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-02-25 05:11:27 -0800

    Formats and styles about page content

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -50,34 +50,45 @@ export const AboutPage = () => {
           <Heading as="h1" size="3xl" mb={2} fontFamily="assistant.normal">About Take Five</Heading>
           <Text fontSize="lg">and <Link href= "https://www.themoviedb.org/" isExternal sx={navTextStyles} borderBottom="2px solid #1962B6">the Movie Database API</Link></Text>
         </Flex>
-        
-        <Box mb={8} p='4' fontFamily="assistant.normal">
-            <Link as={RouterLink} to="/" >
-              <img src={takeFiveLogo} alt="Take Five Logo" width="100%"/>
-            </Link>
-          <Heading as="h2" size="lg" fontFamily="assistant.normal">What is Take Five?</Heading>
-          <Flex justify="center" direction="column" gap="1rem">
+        <Flex direction="column" gap="5" py="5">
+          <Grid justify="center" gridTemplateColumns={gridTempCol} gridTemplateRows={gridTempRow} gap={gap} px={padding}>
+            <Box >
+              <Link as={RouterLink} to="/" >
+                  <img src={takeFiveLogo} alt="Take Five Logo" width="100%" style={{ maxWidth: '150px' }}/>
+              </Link>
+            </Box>
+
+            <Box >
+              <Heading as="h2" fontSize="20" fontFamily="assistant.normal">What is Take Five?</Heading>
+              <Text mb={50} maxW="60rem">
+                Take Five is a web-app designed to help you keep your movie watch-list up-to-date and organized. Stay on top of what movies are new and popular and add movies to your list where you can track what you have and have not yet seen.
+              </Text>
+            </Box>
             
-            <Text>
-              Take Five is a web-app designed to help you keep your movie watch-list up-to-date and organized. Stay on top of what movies are new and popular and add movies to your list where you can track what you have and have not yet seen.
-            </Text>
-          </Flex>
-        </Box>
+          </Grid>
+
+          <Grid justify="center" gridTemplateColumns={gridTempCol} gridTemplateRows={gridTempRow} gap={gap} px={padding}>
+            <Box >
+              <Link href= "https://www.themoviedb.org/" isExternal>
+                <img src={tmdbLogo} alt="The Movie Database Logo" width={tmdbLogoWidth} style={{ maxWidth: '100px' }}/>
+              </Link>
+            </Box>
+
+            <Box >
+              <Heading as="h2" fontSize="20" fontFamily="assistant.normal">What is the Movie Database?</Heading>
+                <Text mb={4} maxW="60rem">
+                  Take Five gets all its movie data and images from The Movie Database (TMDB), a community-built movie and TV database. The TMDB API (application programming interface) is a system that allows Take Five to programmatically keep its information up-to-date. Read more about TMDB on <Link href= "https://www.themoviedb.org/" isExternal sx={navTextStyles} borderBottom="2px solid #1962B6">their website</Link>.
+                </Text>
+            </Box>
+            
+          </Grid>
+        </Flex>
         
-        <Box mb={8} p='4'>
-          <Heading as="h2" size="lg" mb={4}>What is the Movie Database?</Heading>
 
-          <Link href= "https://www.themoviedb.org/" isExternal>
-            <img src={tmdbLogo} alt="The Movie Database Logo" style={{width:'200px', height: '200px'}}/>
-          </Link>
-
-          <Text mb={4}>
-            Take Five gets all its movie data and images from The Movie Database (TMDB), a community-built movie and TV database. The TMDB API (application programming interface) is a system that allows Take Five to programmatically keep its information up-to-date. Read more about TMDB on their website.
-          </Text>
-        </Box>
+        
         
         {/* Disclaimer section */}
-        <Text>
+        <Text align="center" border="2px solid #ffffff22" w="fit-content" p="3" m="15rem auto 0">
           Disclaimer: This product uses the TMDb API but is not endorsed or certified by TMDb.
         </Text>
       </Box>
